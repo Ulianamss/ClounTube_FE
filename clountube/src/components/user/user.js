5a4ca1a651c6e9594cde5fe570131d5109d7cfd2
Ульяна <ulianamss@mail.ru>
2022-02-16 00:28:11 +0300

    no y-scroll

--- clountube/src/components/user/user.js
+++ clountube/src/components/user/user.js
@@ -4,7 +4,7 @@ import './user.css'
 
 export const User = (props)=>{
     return(
-        <div className='container'>
+        <div className='containerUser'>
             <div className='userPicturePageDiv'>
                 <img className='userPicturePage' src={props.props.userPicture}/>
             </div>
